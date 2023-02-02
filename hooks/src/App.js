78b78d7dc93e3ccde6e59b8b41e73dd90e4a0c3e
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-02-01 23:33:21 -0300

    useReducer + useContext

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,19 +1,53 @@
-import logo from './logo.svg';
+import P from 'prop-types';
+import { createContext, useContext, useReducer } from 'react';
 import './App.css';
 
+//data.jsx
+export const globalState = {
+  title: 'Título do contexto',
+  body: 'Corpo do contexto',
+  counter: 0,
+};
+
+//reducer.js
+export const reducer = (state, action) => {
+  console.log(action);
+  return { ...state };
+};
+
+//AppContext.jsx
+export const Context = createContext();
+export const AppContext = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, globalState);
+
+  return (
+    <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
+  );
+};
+
+AppContext.propTypes = {
+  children: P.node,
+};
+
+//H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+
+  return (
+    <h1 onClick={() => context.dispatch({ type: 'CHANGE_TITLE' })}>
+      {context.state.title}
+    </h1>
+  );
+};
+
+//App.jsx
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext>
+      <div>
+        <H1 />
+      </div>
+    </AppContext>
   );
 }
 
